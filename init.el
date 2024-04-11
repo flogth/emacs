70d132583e74f1f64cce07b37ec6dd68c96e20e8
flodobeutlin <flodobeutlin@mailbox.org>
2024-04-11 09:07:29 +0000

    Update from https://github.com/flogth/dotfiles/commit/ab1fd62f4844148c16d33e08603b0d94acf72fb8

--- init.el
+++ init.el
@@ -28,13 +28,6 @@
   (require 'package)
   (package-initialize))
 
-(defmacro set! (&rest args)
-  "Set variables using customize with usage of ARGS like `setq'."
-  (declare (debug setq))
-  `(progn
-     ,@(cl-loop for (key value) on args by #'cddr
-                collecting `(customize-set-variable ',key ,value))))
-
 (let* ((site-lisp-dir (file-name-as-directory (locate-user-emacs-file "site-lisp")))
        (dirs (directory-files site-lisp-dir t "^[^.]")))
   (dolist (d dirs)
@@ -45,52 +38,55 @@
         (byte-recompile-directory d)
         (load autoload-file)))))
 
-(load (set! custom-file (locate-user-emacs-file "custom.el")) t)
+(load (setopt custom-file (locate-user-emacs-file "custom.el")) t)
 
 ;;;; Sane defaults
-(set! inhibit-startup-message t
+(setopt inhibit-startup-message t
       window-resize-pixelwise t
       frame-resize-pixelwise t
       use-dialog-box nil
       use-short-answers t
       visible-bell nil)
 
-(set! backup-directory-alist '((".*" . "~/.local/share/backup"))
+(setopt backup-directory-alist '((".*" . "~/.local/share/backup"))
+      tramp-backup-directory-alist backup-directory-alist
       backup-by-copying t
       delete-old-versions t
       create-lockfiles nil
       auto-save-default nil)
 
-(set! scroll-margin 1
-      scroll-step 1
-      scroll-conservatively 101
-      scroll-preserve-screen-position t
-      fast-but-imprecise-scrolling t)
+(setopt scroll-margin 1
+        scroll-step 1
+        scroll-conservatively 101
+        scroll-preserve-screen-position t
+        fast-but-imprecise-scrolling t
+        pixel-scroll-precision-mode t
+        pixel-scroll-mode t)
 
-(set! help-window-select t)
+(setopt help-window-select t)
 
-(set! calendar-week-start-day 1
+(setopt calendar-week-start-day 1
       calendar-date-style 'iso)
 
-(set! use-package-always-defer t)
+(setopt use-package-always-defer t)
 
 ;;;; ui
 (require-theme 'modus-themes)
-(set! modus-themes-italic-constructs t
+(setopt modus-themes-italic-constructs t
       modus-themes-bold-constructs t)
 (load-theme 'modus-operandi)
 (global-set-key (kbd "<f5>") #'modus-themes-toggle)
 
-(set! column-number-mode t
+(setopt column-number-mode t
       mode-line-compact t
       mode-line-percent-position nil)
 
 ;;;; basic editing
-(set! tab-width 4
+(setopt tab-width 4
       indent-tabs-mode nil)
 
-(set! save-interprogram-paste-before-kill t
-      kill-do-not-save-duplicates)
+(setopt save-interprogram-paste-before-kill t
+        kill-do-not-save-duplicates t)
 
 (use-package avy
   :ensure t
@@ -101,7 +97,7 @@
 
 (use-package paren
   :hook (after-init . show-paren-mode)
-  :init (set! show-paren-delay 0
+  :init (setopt show-paren-delay 0
               show-paren-context-when-offscreen 'overlay))
 
 (use-package vertico
@@ -109,16 +105,16 @@
   :hook (after-init . vertico-mode)
   :bind (:map vertico-map
               ("C-j" . #'vertico-exit-input))
-  :init (set! vertico-cycle t
-              vertico-resize nil))
+  :init (setopt vertico-cycle t
+                vertico-resize nil))
 
 (use-package corfu
   :ensure t
   :hook (prog-mode . corfu-mode)
-  :init (set! corfu-preview-current nil
-              corfu-cycle t
-              corfu-echo-documentation 0.25
-              tab-always-indent 'complete))
+  :init (setopt corfu-preview-current nil
+                corfu-cycle t
+                corfu-echo-documentation 0.25
+                tab-always-indent 'complete))
 
 (use-package consult
   :ensure t
@@ -128,11 +124,11 @@
 
 (use-package orderless
   :ensure t
-  :init (set! completion-styles '(orderless basic)))
+  :init (setopt completion-styles '(orderless basic)))
 
 (use-package savehist
   :hook (after-init . savehist-mode)
-  :init (set! history-delete-duplicates t
+  :init (setopt history-delete-duplicates t
               history-length 1000
               savehist-save-minibuffer-history t))
 
@@ -141,12 +137,12 @@
 
 (use-package recentf
   :bind ("C-x C-r" . recentf)
-  :init (set! recentf-mode t
+  :init (setopt recentf-mode t
               recentf-max-saved-items 128))
 
 ;;;; applications
 (use-package dired
-  :init (set! dired-dwim-target t
+  :init (setopt dired-dwim-target t
               dired-listing-switches "-NAhl --group-directories-first"))
 
 (use-package gnus
@@ -154,53 +150,53 @@
          (gnus-mode . hl-line-mode))
   :bind ("C-c m" . gnus)
   :init
-  (set! gnus-select-method '(nnnil)
-        gnus-parameters
-        '(("^nnimap"
-           (gcc-self . t)
-           (gnus-use-scoring . nil)
-           (display . nil)
-           (agent-predicate . always)))
-        gnus-summary-line-format "%U%R%z%I%(%[%-23,23f%]%) %s\n"
-        mm-discouraged-alternatives '("text/html" "text/richtext")
-        gnus-secondary-select-methods
-        '((nntp "gmane" (nntp-address "news.gmane.io"))
-          (nnimap "uni"
-                  (gnus-search-engine gnus-search-imap)
-                  (nnimap-user "florian.guthmann@fau.de")
-                  (nnimap-address "faumail.fau.de")
-                  (nnimap-stream starttls))
-          (nnimap "personal"
-                  (gnus-search-engine gnus-search-imap)
-                  (nnimap-user "flogth@mailbox.org")
-                  (nnimap-address "imap.mailbox.org")
-                  (nnimap-stream starttls)))
-        gnus-posting-styles
-        '(("" (gcc "nnimap+personal:Sent"))
-          ("uni"
-           (address "florian.guthmann@fau.de")
-           (gcc "nnimap+uni:Sent"))
-          ((header "to" "fau.de")
-           (gcc "nnimap+uni:Sent"))
-          ((header "from" "fau.de")
-           (gcc "nnimap+uni:Sent")))
-        mail-user-agent 'gnus-user-agent
-        user-mail-address "flogth@mailbox.org"
-        user-full-name "Florian Guthmann"
-        message-server-alist
-        '(("florian.guthmann@fau.de" . "smtp faumail.fau.de 587"))
-        send-mail-function #'smtpmail-send-it
-        smtpmail-smtp-server "smtp.mailbox.org"
-        smtpmail-stream-type 'starttls
-        smtpmail-smtp-service 587))
+  (setopt gnus-select-method '(nnnil)
+          gnus-parameters
+          '(("^nnimap"
+             (gcc-self . t)
+             (gnus-use-scoring . nil)
+             (display . nil)
+             (agent-predicate . always)))
+          gnus-summary-line-format "%U%R%z %d %I%(%[%4L: %-23,23f%]%) %s\n"
+          mm-discouraged-alternatives '("text/html" "text/richtext")
+          gnus-secondary-select-methods
+          '((nntp "gmane" (nntp-address "news.gmane.io"))
+            (nnimap "uni"
+                    (gnus-search-engine gnus-search-imap)
+                    (nnimap-user "florian.guthmann@fau.de")
+                    (nnimap-address "faumail.fau.de")
+                    (nnimap-stream starttls))
+            (nnimap "personal"
+                    (gnus-search-engine gnus-search-imap)
+                    (nnimap-user "flogth@mailbox.org")
+                    (nnimap-address "imap.mailbox.org")
+                    (nnimap-stream starttls)))
+          gnus-posting-styles
+          '(("" (gcc "nnimap+personal:Sent"))
+            ("uni"
+             (address "florian.guthmann@fau.de")
+             (gcc "nnimap+uni:Sent"))
+            ((header "to" "fau.de")
+             (gcc "nnimap+uni:Sent"))
+            ((header "from" "fau.de")
+             (gcc "nnimap+uni:Sent")))
+          mail-user-agent 'gnus-user-agent
+          user-mail-address "flogth@mailbox.org"
+          user-full-name "Florian Guthmann"
+          message-server-alist
+          '(("florian.guthmann@fau.de" . "smtp faumail.fau.de 587"))
+          send-mail-function #'smtpmail-send-it
+          smtpmail-smtp-server "smtp.mailbox.org"
+          smtpmail-stream-type 'starttls
+          smtpmail-smtp-service 587))
 
 ;;;; version control
-(set! vc-follow-symlinks t)
+(setopt vc-follow-symlinks t)
 
 (use-package magit
   :ensure t
   :bind ("C-c g" . magit-status)
-  :init (set! magit-define-global-key-bindings nil))
+  :init (setopt magit-define-global-key-bindings nil))
 
 (use-package diff-hl
   :ensure t
@@ -214,33 +210,33 @@
 
 (use-package compile
   :bind ("C-c k" . compile)
-  :init (set! compilation-scroll-output t
+  :init (setopt compilation-scroll-output t
         compilation-ask-about-save nil))
 
 (use-package ansi-color
   :hook (compilation-filter-hook . ansi-color-compilation-filter)
-  :init (set! ansi-color-for-compilation-mode t))
+  :init (setopt ansi-color-for-compilation-mode t))
 
 (use-package eglot
   :bind (:map eglot-mode-map
               ("C-c a" . #'eglot-code-actions)
               ("C-c r" . #'eglot-rename)
               ("C-c f" . #'eglot-format))
-  :init (set! eglot-autoshutdown t
+  :init (setopt eglot-autoshutdown t
               eglot-confirm-server-initiated-edits 'diff))
 
 (use-package eldoc
-  :init (set! eldoc-echo-area-use-multiline-p nil
-              eldoc-idle-delay 0.2))
+  :init (setopt eldoc-echo-area-use-multiline-p nil
+                eldoc-idle-delay 0.2))
 
 
 (use-package xref
-  :init (set! xref-search-program 'ripgrep))
+  :init (setopt xref-search-program 'ripgrep))
 
 ;;;; markup languages
 (use-package auctex
   :ensure t
-  :init (set! TeX-master 'dwim
+  :init (setopt TeX-master 'dwim
               TeX-auto-save t
               TeX-parse-self t
               preview-auto-cache-preamble t
@@ -259,10 +255,11 @@
 (use-package org
   :hook ((org-mode . org-indent-mode)
          (org-mode . visual-line-mode))
-  :init (set! org-pretty-entities nil
+  :init (setopt org-pretty-entities nil
               org-html-doctype "xhtml5"
               org-html-html5-fancy t
-              org-html-htmlize-output-type 'css))
+              org-html-htmlize-output-type 'css
+              org-export-dispatch-use-expert-ui t))
 
 ;;;; programming languages
 
@@ -273,12 +270,12 @@
 
 (use-package proof-general
   :ensure t
-  :init (set! proof-splash-enable nil
+  :init (setopt proof-splash-enable nil
               proof-three-window-enable nil
               proof-script-fly-past-comments t))
 
 (use-package gnu-apl-mode
-  :init (set! gnu-apl-show-tips-on-start nil))
+  :init (setopt gnu-apl-show-tips-on-start nil))
 
 (use-package nix-mode
   :ensure t
@@ -287,12 +284,12 @@
 (use-package sly
   :ensure t
   :hook (lisp-mode . sly)
-  :init (set! inferiour-lisp-program "sbcl"))
+  :init (setopt inferiour-lisp-program "sbcl"))
 
 (use-package haskell-mode
   :ensure t
   :hook (haskell-mode . interactive-haskell-mode)
-  :init (set! haskell-completing-read-function #'completing-read))
+  :init (setopt haskell-completing-read-function #'completing-read))
 
 (use-package sweeprolog
   :ensure t
@@ -300,7 +297,7 @@
   :hook (sweeprolog-mode . sweeprolog-electric-layout-mode))
 
 ;;;; keybindings
-(set! repeat-mode t)
+(setopt repeat-mode t)
 
 (global-set-key (kbd "M-[") #'insert-pair)
 (global-set-key (kbd "M-)") #'delete-pair)
